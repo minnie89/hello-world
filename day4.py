e59918ea8439eb92e48f72b655285a2b72108e3d
Samantha <41205553+minnie89@users.noreply.github.com>
2018-08-06 23:09:27 -0700

    Day 4 added
    Adding files of code from homework for 1MWTT

---
+++ day4.py
@@ -0,0 +1,56 @@
+#99 Bottles of Beer on the Wall
+# bottles = 100
+# while bottles >= 3:
+# 	bottles = bottles - 1
+# 	print(str(bottles) + " bottles of beer on the wall, " + str(bottles) + " bottles of beer, we take one down, pass it around, " + str(bottles - 1) + " bottles of beer on the wall!")
+# 	continue
+# 	break
+# bottles = 1
+# print (str(bottles) + " bottle of beer on the wall, " + str(bottles) + " bottle of beer, we take it down, pass it around, NO MORE bottles of beer on the wall!")
+
+
+# #Deaf Grandma
+# print ("Talk to Grandma!")
+# hello = input("")
+# while hello != hello.upper():
+# 	print ("HUH? SPEAK UP GIRL!")
+# 	hello = input("")	
+# if hello == hello.upper():
+# 		print ("NO, NOT SINCE 1938!")
+	
+	
+# #Leap Years
+# start_year = input()
+# end_year = input()
+
+# for year in range(int(start_year), int(end_year)):
+# 	if year % 4 == 0:
+# 		print (str(year) + " was a leap year.")
+# 	elif year % 100 == 0 and year % 400 == 0:
+# 		print(str(year) + " was a leap year.")
+# 	else: print(str(year) + " was NOT a leap year.")	
+
+#Something in Your Life to Count
+
+
+#Building an Array w/ Sorting
+#MAKE SURE TO FIX BLANK STRING ADDED TO LIST
+
+# my_list = [] #starting with empty list
+# building = True
+
+# while building:
+# 	item = input()
+# 	my_list.append(item)
+# 	if item == "":
+# 		break
+# print(my_list)
+
+# my_list_sorted = sorted(my_list)
+
+# print(my_list_sorted)
+
+#Table of Contents 
+table = ['Chapter 1', 'Chapter 2', 'Chapter 3',
+'Chapter 4', 'Chapter 5', 'Chapter 6', 'Page 1', 'Page 5', 'Page 11',
+'Page 22', 'Page 36', 'Page 48',]
